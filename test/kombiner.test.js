1e3b45ac14604f93b31741e0a1dd583fc2bdaa6a
Folletto <folletto@gmail.com>
2014-04-13 20:11:43 +0100

    Test: added a reverse test.

--- test/kombiner.test.js
+++ test/kombiner.test.js
@@ -4,13 +4,22 @@ var k = require('../index').listen(null);
 
 describe('Kombiner', function() {
   describe('#combine', function() {
-    var out = k.combine([
-      './test/raw-file-a.txt',
-      './test/raw-file-b.txt'
-    ]);
-    
     it('should concatenate in order A-B', function() {
-      assert.equal(out, 'A\nA\nA\nAB\nB\nB\nB', 'Concatenation failed.');
+      var out = k.combine([
+        './test/raw-file-a.txt',
+        './test/raw-file-b.txt'
+      ]);
+      
+      assert.equal(out, 'A\nA\nA\nAB\nB\nB\nB');
+    });
+    
+    it('should concatenate in order B-A', function() {
+      var out = k.combine([
+        './test/raw-file-b.txt',
+        './test/raw-file-a.txt'
+      ]);
+      
+      assert.equal(out, 'B\nB\nB\nBA\nA\nA\nA');
     });
   });
 });
