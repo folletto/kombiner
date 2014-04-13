af846e89159c014fee98d0db00a0b450801c9ad5
Folletto <folletto@gmail.com>
2014-04-13 21:59:28 +0100

    watch() updated ti use watch instead of watchFile.
    In the quick tests I run on my machine watch takes 2sec while watchFile takes 5sec.

--- test/kombiner.test.js
+++ test/kombiner.test.js
@@ -1,6 +1,13 @@
 
 var assert = require('assert');
+var fs = require('fs');
+
 var k = require('../index').listen(null);
+
+var FILE_WATCHED_1 = './test/raw-file-watch-1.txt';
+var FILE_WATCHED_2 = './test/raw-file-watch-2.txt';
+var FILE_WATCHED_3 = './test/raw-file-watch-3.txt';
+
 
 describe('Kombiner', function() {
   describe('#combine', function() {
@@ -21,5 +28,42 @@ describe('Kombiner', function() {
       
       assert.equal(out, 'B\nB\nB\nBA\nA\nA\nA');
     });
+  });
+  
+  describe('#watch', function() {
+    it('should watch one file changing', function(done) {
+      this.timeout(6000);
+       
+      fs.writeFileSync(FILE_WATCHED_1, "Test", 'utf8');
+      k.watch(FILE_WATCHED_1, function(file) {
+        fs.unlink(FILE_WATCHED_1);
+        done();
+      });
+      fs.writeFileSync(FILE_WATCHED_1, Date.now(), 'utf8');
+    });
+    
+    it('should watch 2 files changing', function(done) {
+      this.timeout(6000);
+      var counter = 1;
+      
+      fs.writeFileSync(FILE_WATCHED_2, "Test", 'utf8');
+      fs.writeFileSync(FILE_WATCHED_3, "Test", 'utf8');
+      k.watchAll([FILE_WATCHED_2, FILE_WATCHED_3], function() {
+        counter--;
+        if (counter == 0) {
+          fs.unlink(FILE_WATCHED_2);
+          fs.unlink(FILE_WATCHED_3);
+          done();
+        }
+      })
+      fs.writeFileSync(FILE_WATCHED_2, Date.now(), 'utf8');
+      fs.writeFileSync(FILE_WATCHED_3, Date.now(), 'utf8');
+    });
+  });
+  
+  after(function() {
+    if (fs.exists(FILE_WATCHED_1)) fs.unlink(FILE_WATCHED_1);
+    if (fs.exists(FILE_WATCHED_2)) fs.unlink(FILE_WATCHED_2);
+    if (fs.exists(FILE_WATCHED_3)) fs.unlink(FILE_WATCHED_3);
   });
 });
